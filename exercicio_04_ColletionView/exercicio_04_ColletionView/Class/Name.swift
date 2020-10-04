56535777c1ae3f9d19e4ccdcfa6ec6bbc9761abb
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2020-10-04 11:42:32 -0300

    Inserindo o exercício 4 finalizado

---
+++ exercicio_04_ColletionView/exercicio_04_ColletionView/Class/Name.swift
@@ -0,0 +1,18 @@
+//
+//  Name.swift
+//  exercicio_04_ColletionView
+//
+//  Created by Grazi Berti on 03/10/20.
+//
+
+import Foundation
+
+class Name {
+    var name: String
+    var profileImage: String
+    
+    init(name: String, profileImage: String) {
+        self.name = name
+        self.profileImage = profileImage
+    }
+}
